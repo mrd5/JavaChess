2114eb003bf8b9beb4af98d3b87849efc57474a6
Mathew Dorish <mathewdorish@hotmail.com>
2019-07-02 18:32:46 -0600

    Created the abstract Piece class

--- src/chess/engine/pieces/Piece.java
+++ src/chess/engine/pieces/Piece.java
@@ -1,5 +1,26 @@
-package
+package chess.engine.pieces;
 
-public class Piece
+import chess.engine.Color;
+import chess.engine.board.Board;
+import chess.engine.board.Moves;
+
+import java.util.List;
+
+public abstract class Piece
 {
+    protected final int position;
+    protected final Color color;
+
+    Piece(final int position, final Color color)
+    {
+        this.position = position;
+        this.color = color;
+    }
+
+    public Color getPieceColor() //Pieces are either white or black
+    {
+        return this.color;
+    }
+
+    public abstract List<Moves> getLegalMoves(final Board board); //All of the possible moves a piece can make
 }
