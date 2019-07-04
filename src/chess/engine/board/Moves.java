a44cf1cc7a2715b88f45a633dc468595ef3ef1e3
Mathew Dorish <mathewdorish@hotmail.com>
2019-07-03 20:11:28 -0600

    Created abstract Moves and concrete classes which extend it:

--- src/chess/engine/board/Moves.java
+++ src/chess/engine/board/Moves.java
@@ -1,6 +1,36 @@
 package chess.engine.board;
 
-public class Moves
+import chess.engine.pieces.Piece;
+
+public abstract class Moves
 {
-    /* TODO */
+    final Board board;
+    final Piece movedPiece;
+    final int destination;
+
+    private Moves(final Board board, final Piece movedPiece, final int destination)
+    {
+        this.board = board;
+        this.movedPiece = movedPiece;
+        this.destination = destination;
+    }
+
+    public static final class MajorMove extends Moves
+    {
+        public MajorMove(final Board board, final Piece movedPiece, final int destination)
+        {
+            super(board, movedPiece, destination);
+        }
+    }
+
+    public static final class AttackMove extends Moves
+    {
+        final Piece attackedPiece;
+
+        public AttackMove(final Board board, final Piece movedPiece, final int destination, final Piece attackedPiece)
+        {
+            super(board, movedPiece, destination);
+            this.attackedPiece = attackedPiece;
+        }
+    }
 }
