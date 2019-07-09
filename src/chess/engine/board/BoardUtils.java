0043156c2082cd1d8854b57e2a704cae536a4182
Mathew Dorish <mathewdorish@hotmail.com>
2019-07-09 17:48:58 -0600

    Added initRow method

--- src/chess/engine/board/BoardUtils.java
+++ src/chess/engine/board/BoardUtils.java
@@ -1,9 +1,49 @@
 package chess.engine.board;
 
-public class BoatUtils
+public class BoardUtils
 {
+    public static final boolean[] FIRST_COLUMN = initColumn(0);
+    public static final boolean[] SECOND_COLUMN = initColumn(1);
+    public static final boolean[] SEVENTH_COLUMN = initColumn(6);
+    public static final boolean[] EIGHTH_COLUMN = initColumn(7);
+
+    public static final boolean[] SECOND_ROW = initRow(8);
+    public static final boolean[] SEVENTH_ROW = initRow(48);
+
+    public static final int NUM_TILES = 64;
+    public static final int NUM_TILES_PER_ROW = 8;
+
+    private BoardUtils()
+    {
+        throw new RuntimeException("How????");
+    }
+
     public static boolean isValidCoordinate(int position)
     {
-        return (position >= 0 && position < 64);
+        return (position >= 0 && position < NUM_TILES);
+    }
+
+    private static boolean[] initColumn(int colNum)
+    {
+        final boolean[] column = new boolean[NUM_TILES];
+
+        do
+        {
+            column[colNum]= true;
+            colNum += NUM_TILES_PER_ROW;
+        } while(colNum < NUM_TILES);
+
+        return column;
+    }
+
+    private static boolean[] initRow(int rowNum)
+    {
+        final boolean[] row = new boolean[NUM_TILES];
+        do
+        {
+            row[rowNum] = true;
+            rowNum++;
+        } while(rowNum % NUM_TILES_PER_ROW != 0);
+        return row;
     }
 }
