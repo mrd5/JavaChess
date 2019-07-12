501b7a29d382e53b807866efaa67093e9d6f5d0c
Mathew Dorish <mathewdorish@hotmail.com>
2019-07-12 17:58:49 -0600

    Added calculateKingCastles methods

--- src/chess/engine/player/BlackPlayer.java
+++ src/chess/engine/player/BlackPlayer.java
@@ -3,9 +3,14 @@ package chess.engine.player;
 import chess.engine.Color;
 import chess.engine.board.Board;
 import chess.engine.board.Moves;
+import chess.engine.board.Tile;
 import chess.engine.pieces.Piece;
+import chess.engine.pieces.Rook;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class BlackPlayer extends Player
 {
@@ -30,5 +35,41 @@ public class BlackPlayer extends Player
     public Player getOpponent()
     {
         return this.board.whitePlayer();
+    }
+
+    @Override
+    protected Collection<Moves> calculateKingCastles(Collection<Moves> currentsMoves, Collection<Moves> opponentsMoves)
+    {
+        final List<Moves> kingCastles = new ArrayList<>();
+
+        if (this.king.isFirstMove() && !this.isInCheck())
+        {
+            if (!this.board.getTile(5).isFull() && !this.board.getTile(6).isFull()) //Spaces between king-side rook and king are empty
+            {
+                final Tile rookTile = this.board.getTile(7);
+
+                if (rookTile.isFull() && rookTile.getPiece().isFirstMove())
+                {
+                    if (Player.calculateAttacksOnTile(5, opponentsMoves).isEmpty() &&
+                            Player.calculateAttacksOnTile(6, opponentsMoves).isEmpty() &&
+                            rookTile.getPiece().getPieceType().isRook())
+                    {
+                        kingCastles.add(new Moves.KingSideCastleMove(this.board, this.king, 6, (Rook) rookTile.getPiece(), rookTile.getTileCoordinate(), 5));
+                    }
+                }
+            }
+
+            if (!this.board.getTile(1).isFull() && !this.board.getTile(2).isFull() && !this.board.getTile(3).isFull()) //queen-side rook and inbetween tiles are empty
+            {
+                final Tile rookTile = this.board.getTile(0);
+
+                if (rookTile.isFull() && rookTile.getPiece().isFirstMove())
+                {
+                    kingCastles.add(new Moves.QueenSideCastleMove(this.board, this.king, 2, (Rook) rookTile.getPiece(), rookTile.getTileCoordinate(), 3));
+                }
+            }
+        }
+
+        return ImmutableList.copyOf(kingCastles);
     }
 }
--- src/chess/engine/player/WhitePlayer.java
+++ src/chess/engine/player/WhitePlayer.java
@@ -3,9 +3,14 @@ package chess.engine.player;
 import chess.engine.Color;
 import chess.engine.board.Board;
 import chess.engine.board.Moves;
+import chess.engine.board.Tile;
 import chess.engine.pieces.Piece;
+import chess.engine.pieces.Rook;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class WhitePlayer extends Player
 {
@@ -30,5 +35,41 @@ public class WhitePlayer extends Player
     public Player getOpponent()
     {
         return this.board.blackPlayer();
+    }
+
+    @Override
+    protected Collection<Moves> calculateKingCastles(Collection<Moves> currentsMoves, Collection<Moves> opponentsMoves)
+    {
+        final List<Moves> kingCastles = new ArrayList<>();
+
+        if (this.king.isFirstMove() && !this.isInCheck())
+        {
+            if (!this.board.getTile(61).isFull() && !this.board.getTile(62).isFull()) //Spaces between king-side rook and king are empty
+            {
+                final Tile rookTile = this.board.getTile(63);
+
+                if (rookTile.isFull() && rookTile.getPiece().isFirstMove())
+                {
+                    if (Player.calculateAttacksOnTile(61, opponentsMoves).isEmpty() &&
+                        Player.calculateAttacksOnTile(62, opponentsMoves).isEmpty() &&
+                        rookTile.getPiece().getPieceType().isRook())
+                    {
+                        kingCastles.add(new Moves.KingSideCastleMove(this.board, this.king, 62, (Rook) rookTile.getPiece(), rookTile.getTileCoordinate(), 59));
+                    }
+                }
+            }
+
+            if (!this.board.getTile(59).isFull() && !this.board.getTile(58).isFull() && !this.board.getTile(57).isFull()) //queen-side rook and inbetween tiles are empty
+            {
+                final Tile rookTile = this.board.getTile(56);
+
+                if (rookTile.isFull() && rookTile.getPiece().isFirstMove())
+                {
+                    kingCastles.add(new Moves.QueenSideCastleMove(this.board, this.king, 58, (Rook) rookTile.getPiece(), rookTile.getTileCoordinate(), 59));
+                }
+            }
+        }
+
+        return ImmutableList.copyOf(kingCastles);
     }
 }
