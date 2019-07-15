1b31df9e637ab18dc7e0e9992962c29f009bc078
Mathew Dorish <mathewdorish@hotmail.com>
2019-07-14 20:22:37 -0600

    Implemented two initialize... methods

--- src/chess/engine/board/BoardUtils.java
+++ src/chess/engine/board/BoardUtils.java
@@ -1,5 +1,11 @@
 package chess.engine.board;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class BoardUtils
 {
     public static final boolean[] FIRST_COLUMN = initColumn(0);
@@ -15,6 +21,10 @@ public class BoardUtils
     public static final boolean[] THIRD_ROW = initRow(40);
     public static final boolean[] SECOND_ROW = initRow(48);
     public static final boolean[] FIRST_ROW = initRow(56);
+
+    public static final String[] ALGEBREIC_NOTATION = initializeAlgebreicNotation();
+    public static final Map<String, Integer> POSITION_TO_COORDINATE = initializePositionToCoordinateMap();
+
 
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
@@ -51,5 +61,42 @@ public class BoardUtils
             rowNum++;
         } while(rowNum % NUM_TILES_PER_ROW != 0);
         return row;
+    }
+
+    private static String[] initializeAlgebreicNotation()
+    {
+        return new String[]
+        {
+                "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+                "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+                "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+                "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+                "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+                "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+                "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+                "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+        };
+    }
+
+    private static Map<String,Integer> initializePositionToCoordinateMap()
+    {
+        final Map<String, Integer> positionToCoordinate = new HashMap<>();
+
+        for (int i = 0; i < NUM_TILES; i++)
+        {
+            positionToCoordinate.put(ALGEBREIC_NOTATION[i], i);
+        }
+
+        return ImmutableMap.copyOf(positionToCoordinate);
+    }
+
+    public static int getCoordinateAtPosition(String position)
+    {
+        return POSITION_TO_COORDINATE.get(position);
+    }
+
+    public static String getPositionAtCoordinate(int destination)
+    {
+        return ALGEBREIC_NOTATION[destination];
     }
 }
